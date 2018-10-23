ebafa808ba684a86e5520b7afed315218c61b33c
Erik Sharp <mr.erik.sharp@gmail.com>
2018-10-23 12:52:54 -0500

    Scaffolding for the database

--- DataAccessLayerTests/MyFirstTestFixture.cs
+++ DataAccessLayerTests/MyFirstTestFixture.cs
@@ -5,8 +5,26 @@ using Xunit;
 
 namespace DataAccessLayerTests
 {
-    public class MyFirstTestFixture
+    public class MyFirstTestFixture : IDisposable
     {
+        private int _counter;
+        private string _phrase;
+        public MyFirstTestFixture()
+        {
+            _counter = 0;
+            _phrase = "hello";
+        }
 
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void ShouldEqualZero()
+        {
+            Assert.Equal(0, _counter);
+            Assert.Equal("hello", _phrase);
+            Assert.Equal("Hello", _phrase, ignoreCase: true);
+        }
     }
 }
